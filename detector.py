e043b88b8f4369338a0e0d4905c08b37db42874f
Jeff <jsp282@cornell.edu>
2016-03-21 13:32:46 -0400

    Added processing code Version 5

--- detector.py
+++ detector.py
@@ -5,48 +5,24 @@ import datetime
 from matplotlib.dates import drange
 
 
-
-#____________________
-
-# lines1  = []
-#
-# #Build List
-# with open('record.csv') as f:
-#    for line1 in f:
-#        if line1!='\n':
-#            if 'Name' not in line1:
-#                if 'New_York' not in line1:
-#                 lines1.append(line1.replace("\n","").split(","))
-#
-#
-# #Name | Device Type | IP | MAC | Last Seen
-#
-#
-# mac_list1 = []
-# for m1 in lines1:
-#     mac1 = m1[3]
-#     if mac1 not in mac_list1:
-#         mac_list1.append(mac1)
-#
-# print len(mac_list1)
-#_________________
-
-#____________________
-
-lines2  = []
-refined = []
-i = 0
-#Build List
-with open('record3_10.csv') as f:
-   lines2 = f.read().splitlines()
+#Builds a list from the file input
+def buildList(file):
+    lines  = []
+    refined = []
+    i = 0
+    #Build List
+    with open(file) as f:
+        lines = f.read().splitlines()
 
 
-#Refine List
-for m in lines2:
-    if ',,' not in m:
-        if 'Seen' not in m:
-            refined.append(m.split(","))
+    #Refine List
+    for m in lines:
+        if ',,' not in m:
+            if 'Seen' not in m:
+                refined.append(m.split(","))
 
+
+    return refined
 
 
 
@@ -54,11 +30,18 @@ for m in lines2:
 #MAC at location 3
 #Name | Device Type | IP | MAC | Last Seen
 
+def countUniqueMac(refined):
+    mac_list1 = []
+    for m1 in refined:
+        mac1 = m1[3]
+        if mac1 not in mac_list1:
+            mac_list1.append(mac1)
 
-mac_list1 = []
-for m1 in refined:
-    mac1 = m1[3]
-    if mac1 not in mac_list1:
-        mac_list1.append(mac1)
+    print len(mac_list1)
 
-print len(mac_list1)
+
+
+
+
+countUniqueMac(buildList('record2.csv'))
+countUniqueMac(buildList('record3_10.csv'))
