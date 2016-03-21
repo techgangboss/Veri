7f4f05faed3665648685231a69dd6290a3d0b803
Jeff <jsp282@cornell.edu>
2016-03-21 13:49:19 -0400

    Added processing code Version 6

--- detector.py
+++ detector.py
@@ -6,6 +6,7 @@ from matplotlib.dates import drange
 
 
 #Builds a list from the file input
+#List of Instances, not MAC
 def buildList(file):
     lines  = []
     refined = []
@@ -26,22 +27,55 @@ def buildList(file):
 
 
 
-#Check unique addresses
-#MAC at location 3
+#Build list of unique addresses
 #Name | Device Type | IP | MAC | Last Seen
 
-def countUniqueMac(refined):
-    mac_list1 = []
-    for m1 in refined:
-        mac1 = m1[3]
-        if mac1 not in mac_list1:
-            mac_list1.append(mac1)
+def buildMacList(refined):
+    mac_list = []
+    time_list = []
+    for rline in refined:
+        if rline[3] not in mac_list:
+            mac_list.append(rline[3])
 
-    print len(mac_list1)
+            time_inst = []
+            time_inst_times = []
+            time_inst.append(rline[0])
+            time_inst.append(rline[1])
+            time_inst.append(rline[2])
+            time_inst.append(rline[3])
+            time_inst_times.append(rline[4])
+            time_inst.append(time_inst_times)
+            time_list.append(time_inst)
+
+        else:
+            for tm_el in time_list:
+                tm_el[4].append(rline[4])
 
 
 
 
+    # for z in range(3):
+    #     if(len(time_list[z][4])>2):
+    #         print(time_list[z])
 
-countUniqueMac(buildList('record2.csv'))
-countUniqueMac(buildList('record3_10.csv'))
+    print(len(time_list))
+
+    return mac_list,time_list
+
+
+#Compare multiple files
+
+def getSuperList(mac_list1,mac_list2):
+    superList = []
+    for q in mac_list1:
+        if q in mac_list2:
+            superList.append(q)
+
+    print(len(superList))
+    pass
+
+
+list1 = buildList('record2.csv')
+list2 = buildList('record3_10.csv')
+u1,t1 = buildMacList(list1)
+u2,t2 = buildMacList(list2)
