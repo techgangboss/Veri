5df6ae515224f674761aab0d38f3cf7cf292acec
Jeff <jsp282@cornell.edu>
2016-03-21 12:52:41 -0400

    Added processing code Version 3

--- detector.py
+++ detector.py
@@ -19,41 +19,46 @@ with open('record.csv') as f:
 #Name | Device Type | IP | MAC | Last Seen
 
 #Build record for one
-personOne = []
-personOneIdentify = []
-personOneTimes = []
+def buildRecord(lines, z):
+    personOne = []
+    personOneIdentify = []
+    personOneTimes = []
 
-personOneIdentify.append(lines[0][0])
-personOneIdentify.append(lines[0][1])
-personOneIdentify.append(lines[0][2])
-personOneIdentify.append(lines[0][3])
-
-
-for m in lines:
-    if m[2] == personOneIdentify[2]:
-        personOneTimes.append(m[4])
-
-personOne.append(personOneIdentify)
-personOne.append(personOneTimes)
-
-# print len(personOneTimes)
-
-# print personOne
+    personOneIdentify.append(lines[0][0])
+    personOneIdentify.append(lines[0][1])
+    personOneIdentify.append(lines[0][2])
+    personOneIdentify.append(lines[0][3])
 
 
+    for m in lines:
+        if m[2] == personOneIdentify[2]:
+            personOneTimes.append(m[4])
 
-timelist = []
-for r in personOneTimes:
-    r1 = r.split(" ")[1]
-    timelist.append(parser.parse(r))
+    personOne.append(personOneIdentify)
+    personOne.append(personOneTimes)
+
+    # print len(personOneTimes)
+
+    # print personOne
 
 
-x = [1]*len(timelist)
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-# start = datetime.datetime.strptime("21-06-2014", "%d-%m-%Y")
-# end = datetime.datetime.strptime("07-07-2014", "%d-%m-%Y")
-# date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
-# #ax.set_xticks()
-plt.scatter(timelist,x)
+
+    timelist = []
+
+    for r in personOneTimes:
+        r1 = r.split(" ")[1]
+        timelist.append(parser.parse(r))
+
+
+    x = [1]*len(timelist)
+    return x
+
+usrlist = []
+rcd = []
+for z in range(len(lines)):
+    rcd.append(z)
+    usrlist.append(buildRecord(lines,z))
+
+
+plt.scatter(usrlist[0],rcd[0])
 plt.show()
